85845fac74d3e749ac380e7c9d6a20ab65d3efc2
Simon Skinner <vultuk@me.com>
2011-10-25 20:29:52 +0100

    Namespaced and made it possible to move to any specified address

--- jquery.gmapsFineTuner.js
+++ jquery.gmapsFineTuner.js
@@ -3,85 +3,107 @@
 // .gmapsFineTuner-gmapDiv = Div holding the Google Map;
 
 (function( $ ) {
-    var divID = 0;
-    
-    var methods = {
+     var methods = {
         init : function( options ) {
+            var divID = 0;
             
             // Default settings for the plugin
-            var defaults = {
-                'startLatLng' : { 'lat' : -34.397, 'lng' : 150.644 },
-                'startZoom' : 8,
-                'country' : "Ireland",
-                'countyPrefix' : '',
-                'counties' : '',
-                'fineTune' : true,
-            };
+             var defaults = {
+                 'startLatLng' : { 'lat' : -34.397, 'lng' : 150.644 },
+                 'startZoom' : 6,
+                 'country' : "Ireland",
+                 'countyPrefix' : '',
+                 'counties' : '',
+                 'fineTune' : true,
+             };
 
-            var radarDefaults = { clickable : false, radius: 1400, };
-            var markerDefaults = { draggable: true, visible : false, }
+             var radarDefaults = { clickable : false, radius: 1400, };
+             var markerDefaults = { draggable: true, visible : false, }
 
-            // Extend the options with the plugin defaults
-            var options = $.extend(defaults, options);
-            options.radar = $.extend(radarDefaults, options.radar);
-            options.marker = $.extend(markerDefaults, options.marker);
-
+             // Extend the options with the plugin defaults
+             var options = $.extend(defaults, options);
+             options.radar = $.extend(radarDefaults, options.radar);
+             options.marker = $.extend(markerDefaults, options.marker);
+            
             // Activate the plugin
             return this.each(function() {
                 divID++;
                 var $this = $(this);
-                var $formDiv = $('<div class="gmapsFineTuner-formDiv"></div>');
-                var $gmapDiv = $('<div id="gmapsFineTuner-map'+divID+'" class="gmapsFineTuner-gmapDiv"></div>');
-                var $countiesBox, $townsBox, $streetBox, $coordsBox;
-                var fullAddress = "", chosenCounty = "", chosenTown = "", chosenStreet = "";
-
+                
                 var gmapsFineTunerGeocoder, gmapsFineTunerMap, gmapsFineTunerLastPositon, gmapsFineTunerMarker, gmapsFineTunerRadar;
 
+                // Namespace all the data that I need to pass between functions
 
+                if ( ! data ) {
+                    $this.data('gmapsFineTuner', {
+                        formDiv : $('<div class="gmapsFineTuner-formDiv"></div>'),
+                        gmapDiv : $('<div id="gmapsFineTuner-map'+divID+'" class="gmapsFineTuner-gmapDiv"></div>'),
+                        coordsBox : $('<input type="hidden" name="gmapsFineTunerPosition'+divID+'">'),
+                        fullAddress : "ss",
+                        chosenCounty : "",
+                        chosenTown : "",
+                        chosenStreet : "",
+                        options : options,
+                        countiesBox : '', 
+                        townsBox : '',
+                        streetBox : '',
+                        gmapsFineTunerGeocoder : '',
+                        gmapsFineTunerMap : '',
+                        gmapsFineTunerLastPositon : '',
+                        gmapsFineTunerMarker : '',
+                        gmapsFineTunerRadar : '',
+                    })
+                }
+                var data = $this.data('gmapsFineTuner');
+                
+                
+                
                 // BEGIN THE COUNTIES BOX
-                if (options.counties.length > 0) {
+                if (data.options.counties.length > 0) {
                     // Counties have been set in the options
-                    $countiesBox = $('<select><option value="-" selected>Please choose the county</option></select');
-                    $.each(options.counties, function(val, text) {
-                        $countiesBox.append(
-                            $('<option></option>').val(options.countyPrefix + text).html(text)
+                    data.countiesBox = $('<select><option value="-" selected>Please choose the county</option></select');
+                    $.each(data.options.counties, function(val, text) {
+                        data.countiesBox.append(
+                            $('<option></option>').val(data.options.countyPrefix + text).html(text)
                         );
                     })
                 } else {
                     // No Counties have been specified
-                    $countiesBox = $('<input type="text" value="County">');
+                    data.countiesBox = $('<input type="text" value="County">');
                 }
                 // Do something when the counties box is changed
-                $countiesBox.change(function() {
+                data.countiesBox.change(function() {
                     if ($(this).find(":selected").val() != "-") {
-                        chosenCounty = (typeof($(this).find(":selected").val()) == "undefined") ? $(this).val() : $(this).find(":selected").val();
-                        chosenStreet = "";
-                        chosenTown = "";
-                        gmapsFineTunerMarker.setVisible(false);
-                        $townsBox.val('Town').show().focus();
-                        $streetBox.hide();
-                        moveMap(9);
+                        //data.chosenCounty = (typeof($(this).find(":selected").val()) == "undefined") ? $(this).val() : $(this).find(":selected").val();
+                        $this.gmapsFineTuner('setCounty', (typeof($(this).find(":selected").val()) == "undefined") ? $(this).val() : $(this).find(":selected").val());
+                        data.chosenStreet = "";
+                        data.chosenTown = "";
+                        data.gmapsFineTunerMarker.setVisible(false);
+                        data.townsBox.val('Town').show().focus();
+                        data.streetBox.hide();
+                        $this.gmapsFineTuner('moveMap', 9);
                     }
                 });
                 // END THE COUNTIES BOX
 
 
                 // BEGIN THE TOWNS BOX
-                $townsBox = $('<input type="text" value="Town">').hide();
+                data.townsBox = $('<input type="text" value="Town">').hide();
                 // Do something when the towns box is changed
-                $townsBox.change(function() {
-                    chosenTown = $(this).val() + ", ";
-                    chosenStreet = "";
-                    gmapsFineTunerMarker.setVisible(false);
-                    $streetBox.val('Address').show().focus();
-                    moveMap(14);
+                data.townsBox.change(function() {
+                    //data.chosenTown = $(this).val() + ", ";
+                    $this.gmapsFineTuner('setTown', $(this).val());
+                    data.chosenStreet = "";
+                    data.gmapsFineTunerMarker.setVisible(false);
+                    data.streetBox.val('Address').show().focus();
+                    $this.gmapsFineTuner('moveMap', 14);
                 });
-                $townsBox.keypress(function (e) { 
+                data.townsBox.keypress(function (e) { 
                     var code = null; 
                     code = (e.keyCode ? e.keyCode : e.which);
 
                     if (code == 13) {
-                        $townsBox.change();
+                        data.townsBox.change();
                     }
 
                     return (code == 13) ? false : true; });
@@ -89,108 +111,129 @@
 
 
                 // BEGIN THE STREET BOX
-                $streetBox = $('<input type="text" value="Address">').hide();
+                data.streetBox = $('<input type="text" value="Address">').hide();
                 // Do something when the towns box is changed
-                $streetBox.change(function() {
-                    chosenStreet = $(this).val() + ", ";
-                    moveMap(17);
+                data.streetBox.change(function() {
+                    //data.chosenStreet = $(this).val() + ", ";
+                    $this.gmapsFineTuner('setStreet', $(this).val());
+                    $this.gmapsFineTuner('moveMap', 17);
                 });
-                $streetBox.keypress(function (e) { 
+                data.streetBox.keypress(function (e) { 
                     var code = null; 
                     code = (e.keyCode ? e.keyCode : e.which);
 
                     if (code == 13) {
-                        $streetBox.change();
+                        data.streetBox.change();
                     }
 
                     return (code == 13) ? false : true; });
                 // END THE STREET BOX
 
-                $coordsBox = $('<input type="hidden" name="gmapsFineTunerPosition'+divID+'">');
 
                 // Add the form to our form div
-                $formDiv.append($countiesBox);
-                $formDiv.append($townsBox);
-                $formDiv.append($streetBox);
+                data.formDiv.append(data.countiesBox);
+                data.formDiv.append(data.townsBox);
+                data.formDiv.append(data.streetBox);
 
 
-                $this.append($formDiv);
-                $this.append($gmapDiv);
-                $this.append($coordsBox);
+                $this.append(data.formDiv);
+                $this.append(data.gmapDiv);
+                $this.append(data.coordsBox);
 
-                createMap();
-
-                // Functions for use with this plugin
-                function createMap() {
-                    gmapsFineTunerGeocoder = new google.maps.Geocoder();
-                    var latlng = new google.maps.LatLng(options.startLatLng.lat, options.startLatLng.lng);
-                    var myOptions = {
-                      zoom: options.startZoom,
-                      center: latlng,
-                      mapTypeId: google.maps.MapTypeId.ROADMAP
-                    };
-                    gmapsFineTunerMap = new google.maps.Map(document.getElementById( $gmapDiv.attr('id') ),
-                        myOptions);
-
-                    gmapsFineTunerRadar = new google.maps.Circle( options.radar );
-                    gmapsFineTunerMarker = new google.maps.Marker( options.marker );
-                    gmapsFineTunerMarker.setMap(gmapsFineTunerMap);
-                    google.maps.event.addListener(gmapsFineTunerMarker, 'dragend', function(event) {
-                        $coordsBox.val(event.latLng.toUrlValue());
-                    });
-
-                    google.maps.event.addListener(gmapsFineTunerMap, 'click', function(event) {
-                        $coordsBox.val(event.latLng.toUrlValue());
-                        gmapsFineTunerMarker.setPosition(event.latLng);
-                        gmapsFineTunerMap.setCenter(event.latLng);
-                        gmapsFineTunerMap.setZoom(17);
-                        gmapsFineTunerMarker.setVisible();
-                        gmapsFineTunerRadar.setMap(null);
-                    });
-
-                }
-
-                function moveMap(zoom) {
-                    zoom = (typeof(zoom) == "undefined") ? options.startZoom : zoom;
-                    // Moves the map to the required location
-                    fullAddress = chosenStreet + chosenTown + chosenCounty + ", " + options.country
-
-
-                    gmapsFineTunerGeocoder.geocode( { 'address': fullAddress}, function(results, status) {
-
-                        if (status == google.maps.GeocoderStatus.OK) {
-                            gmapsFineTunerLastPositon = results[0].geometry.location;
-                        }
-
-                        gmapsFineTunerMap.setCenter(gmapsFineTunerLastPositon);
-                        gmapsFineTunerMarker.setPosition(gmapsFineTunerLastPositon);
-
-                        $coordsBox.val(gmapsFineTunerLastPositon.toUrlValue());
-
-                        if (results[0].geometry.location_type == "APPROXIMATE") {
-                            // This is only approximate so lets give them a radar so they can
-                            // click to add their own specific point!
-                            gmapsFineTunerRadar.setCenter(gmapsFineTunerLastPositon);
-                            gmapsFineTunerRadar.setMap(gmapsFineTunerMap);
-
-                            gmapsFineTunerRadar.setOptions( { radius : (zoom == 9) ? 35000 : 1100 } )
-
-                        } else {
-                            // We are pretty sure this is where they want to be so lets drop a marker!
-                            gmapsFineTunerMarker.setVisible();
-                            gmapsFineTunerRadar.setMap(null);
-                        }
-
-                        gmapsFineTunerMap.setZoom(zoom);
-
-
-                    });
-
-
-                }
+                $this.gmapsFineTuner('createMap');
+                
 
             });
         },
+        setLocation : function( street, town, county ) {
+            $(this).gmapsFineTuner('setCounty', county);
+            $(this).gmapsFineTuner('setTown', town);
+            $(this).gmapsFineTuner('setStreet', street);
+            
+            $(this).gmapsFineTuner('moveMap', 17);
+        },
+        setCounty : function( county ) {
+            var data = $(this).data('gmapsFineTuner');
+            data.chosenCounty = county;
+        },
+        setTown : function( town ) {
+            var data = $(this).data('gmapsFineTuner');
+            data.chosenTown = town + ", ";
+        },
+        setStreet : function( street ) {
+            var data = $(this).data('gmapsFineTuner');
+            data.chosenStreet = street + ", ";
+        },
+        createMap : function() {
+            var data = $(this).data('gmapsFineTuner');
+            data.gmapsFineTunerGeocoder = new google.maps.Geocoder();
+            var latlng = new google.maps.LatLng(data.options.startLatLng.lat, data.options.startLatLng.lng);
+            var myOptions = {
+              zoom: data.options.startZoom,
+              center: latlng,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            data.gmapsFineTunerMap = new google.maps.Map(document.getElementById( data.gmapDiv.attr('id') ),
+                myOptions);
+
+            data.gmapsFineTunerRadar = new google.maps.Circle( data.options.radar );
+            data.gmapsFineTunerMarker = new google.maps.Marker( data.options.marker );
+            data.gmapsFineTunerMarker.setMap(data.gmapsFineTunerMap);
+            google.maps.event.addListener(data.gmapsFineTunerMarker, 'dragend', function(event) {
+                data.coordsBox.val(event.latLng.toUrlValue());
+            });
+
+            google.maps.event.addListener(data.gmapsFineTunerMap, 'click', function(event) {
+                data.coordsBox.val(event.latLng.toUrlValue());
+                data.gmapsFineTunerMarker.setPosition(event.latLng);
+                data.gmapsFineTunerMap.setCenter(event.latLng);
+                data.gmapsFineTunerMap.setZoom(17);
+                data.gmapsFineTunerMarker.setVisible();
+                data.gmapsFineTunerRadar.setMap(null);
+            });
+            
+        },
+        moveMap : function( zoom ) {
+            var data = $(this).data('gmapsFineTuner');
+            //function moveMap(zoom) {
+                zoom = (typeof(zoom) == "undefined") ? data.options.startZoom : zoom;
+                // Moves the map to the required location
+                data.fullAddress = data.chosenStreet + data.chosenTown + data.chosenCounty + ", " + data.options.country;
+
+
+                data.gmapsFineTunerGeocoder.geocode( { 'address': data.fullAddress}, function(results, status) {
+
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        data.gmapsFineTunerLastPositon = results[0].geometry.location;
+                    }
+
+                    data.gmapsFineTunerMap.setCenter(data.gmapsFineTunerLastPositon);
+                    data.gmapsFineTunerMarker.setPosition(data.gmapsFineTunerLastPositon);
+
+                    data.coordsBox.val(data.gmapsFineTunerLastPositon.toUrlValue());
+
+                    if (results[0].geometry.location_type == "APPROXIMATE") {
+                        // This is only approximate so lets give them a radar so they can
+                        // click to add their own specific point!
+                        data.gmapsFineTunerRadar.setCenter(data.gmapsFineTunerLastPositon);
+                        data.gmapsFineTunerRadar.setMap(data.gmapsFineTunerMap);
+
+                        data.gmapsFineTunerRadar.setOptions( { radius : (zoom == 9) ? 35000 : 1100 } )
+
+                    } else {
+                        // We are pretty sure this is where they want to be so lets drop a marker!
+                        data.gmapsFineTunerMarker.setVisible();
+                        data.gmapsFineTunerRadar.setMap(null);
+                    }
+
+                    data.gmapsFineTunerMap.setZoom(zoom);
+
+
+                });
+
+
+            //}
+        }
       };
     
     
